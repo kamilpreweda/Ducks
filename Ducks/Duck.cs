83dec490837e37728a823150ae2eb6810d026dd2
Kamil Preweda <preweda.kamil@o2.pl>
2022-06-06 08:27:49 +0200

    Added 'Duck' class and 'KindOfDuck' enum

---
+++ Ducks/Duck.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ducks
+{
+    internal class Duck
+    {
+        public int Size { get; set; }
+        public KindOfDuck Kind { get; set; }
+    }
+}
