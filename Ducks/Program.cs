d26fc027701c8731ac03ca79d1bb65e32b185f99
Kamil Preweda <preweda.kamil@o2.pl>
2022-06-06 08:34:19 +0200

    Added 'List<Duck> ducks'

--- Ducks/Program.cs
+++ Ducks/Program.cs
@@ -14,7 +14,15 @@ namespace Ducks
         }
         static void Main(string[] args)
         {
-            
+            List<Duck> ducks = new List<Duck>()
+            {
+                new Duck() { Kind = KindOfDuck.krzyżówka, Size = 43 },
+                new Duck() { Kind = KindOfDuck.piżmowa, Size = 45 },
+                new Duck() { Kind = KindOfDuck.nur, Size = 35 },
+                new Duck() { Kind = KindOfDuck.piżmowa, Size = 27 },
+                new Duck() { Kind = KindOfDuck.krzyżówka, Size = 35 },
+                new Duck() { Kind = KindOfDuck.nur, Size = 33 },
+            };
         }
     } 
 }
